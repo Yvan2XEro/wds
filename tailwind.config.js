16b8717a78adadcd22e0b8866c33566178825f51
Yvan2Ero <yvankanaj@gmail.com>
2022-07-17 10:23:07 +0100

    Mise en place de la structure du projet

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/common/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+        'primary-300': '#6869f7',
+        'primary-500': '#211d58',
+        'pink': '#e432a2',
+      }
+    },
+  },
+  plugins: [],
+}
