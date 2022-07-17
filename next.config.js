6a7086b579a80e67b896bcbca7d4d65c0302585e
Yvan2Ero <yvankanaj@gmail.com>
2022-07-17 13:03:00 +0100

    Design home 1

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
+  experimental: {
+    images: {
+      allowFutureImage: true,
+    },
+  }
 }
 
 module.exports = nextConfig
