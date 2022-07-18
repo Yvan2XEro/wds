8f81df9af93707537ff52c251a18d8448526338a
Yvan2Ero <yvankanaj@gmail.com>
2022-07-18 23:45:14 +0100

    Animation des images

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -1,6 +1,7 @@
 import Footer from "@components/layouts/Footer";
 import Header from "@components/shared/Header";
 import Input from "@components/shared/Input";
+import { motion } from "framer-motion";
 import type { NextPage } from "next";
 import Head from "next/head";
 
@@ -16,24 +17,45 @@ const Contacts: NextPage = () => {
         <Header title="Contacts" />
         <div className="relative grid grid-cols-1 md:grid-cols-2">
           <div className="relative min-h-[420px]">
-            <div className="absolute top-[15px] left-[120px] md:left-[180px] flex p-2 border rounded-full bg-[white] drop-shadow-lg w-[250px] h-[250px]">
+            <motion.div
+              animate={{
+                transition: { yoyo: Infinity, duration: 3 },
+                x: 5,
+                y: -10,
+              }}
+              className="absolute top-[15px] left-[120px] md:left-[180px] flex p-2 border rounded-full bg-[white] drop-shadow-lg w-[250px] h-[250px]"
+            >
               <img
                 src="https://gitlab.com/uploads/-/system/user/avatar/6049371/avatar.png"
                 className="rounded-full"
               />
-            </div>
-            <div className="absolute top-[250px] left-[30px] flex p-2 border rounded-full bg-[white] drop-shadow-lg  w-[150px] h-[150px]">
+            </motion.div>
+            <motion.div
+              animate={{
+                transition: { yoyo: Infinity, duration: 3 },
+                x: 15,
+                y: 26,
+              }}
+              className="absolute top-[250px] left-[30px] flex p-2 border rounded-full bg-[white] drop-shadow-lg  w-[150px] h-[150px]"
+            >
               <img
                 src="https://gitlab.com/uploads/-/system/user/avatar/6049371/avatar.png"
                 className="rounded-full"
               />
-            </div>
-            <div className="absolute top-[-20px] left-[15px] flex p-2 border rounded-full bg-[white] drop-shadow-lg w-[100px] h-[100px]">
+            </motion.div>
+            <motion.div
+              animate={{
+                transition: { yoyo: Infinity, duration: 3 },
+                x: 36,
+                y: -10,
+              }}
+              className="absolute top-[-20px] left-[15px] flex p-2 border rounded-full bg-[white] drop-shadow-lg w-[100px] h-[100px]"
+            >
               <img
                 src="https://gitlab.com/uploads/-/system/user/avatar/6049371/avatar.png"
                 className="rounded-full"
               />
-            </div>
+            </motion.div>
           </div>
           <form className="relative md:mx-20 ]">
             <div className="sm:px-4">
