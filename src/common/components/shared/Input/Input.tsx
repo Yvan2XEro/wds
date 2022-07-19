76669ab1b9be5892e8fee6604cac359f2f3e24a9
Yvan2Ero <yvankanaj@gmail.com>
2022-07-19 10:21:01 +0100

    Retouche de la page de contacts

---
+++ src/common/components/shared/Input/Input.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export type InputPropsType = {
+  id?: string;
+  type?: string;
+  min?: number;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  label: string;
+  className?: string;
+};
+export function Input({
+  id = "",
+  type = "text",
+  label,
+  value,
+  min = 0,
+  onChange,
+  placeholder = "",
+  className = "",
+}: InputPropsType) {
+  return (
+    <div className="mb-6">
+      <label htmlFor={id} className="block mb-2 text-sm font-medium">
+        {label}
+      </label>
+      <input
+        type={type}
+        id={id}
+        min={min}
+        // value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className={
+          "bg-gray-50 border border-gray-300 text-sm rounded-lg focus:border-primary-300 block w-full p-2.5 " +
+          className
+        }
+        required
+      />
+    </div>
+  );
+}
---
+++ src/common/components/shared/Input/Select.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { InputPropsType } from "./Input";
+
+type SelectPropsType = InputPropsType & {
+  options: Array<{ label: string; value: string }>;
+  id?: string;
+  type?: string;
+  onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+};
+export function Select({
+  id = "",
+  type = "",
+  label,
+  value,
+  onChange,
+  placeholder = "",
+  className = "",
+  options,
+}: SelectPropsType) {
+  return (
+    <div className="mb-6">
+      <label htmlFor={id} className="block mb-2 text-sm font-medium">
+        {label}
+      </label>
+      <select
+        id={id}
+        onChange={onChange}
+        placeholder={placeholder}
+        value={value}
+        className={
+          "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-300 focus:border-primary-300 block w-full p-2.5 " +
+          className
+        }
+      >
+        {options.map((o) => (
+          <option value={o.value} key={o.label} selected>
+            {o.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
